f01e7a2be274f07338de0db0d672ed8b57817d8c
Mo <Mo@Folah-MacBook-Pro.local>
2023-07-06 13:53:46 -0700

    okay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Form from './components/Form';
 // import FruitList from './components/FruitList';
 // import Product from './components/Product';
 // import ConditionalComponent from './components/ConditionalComponent';
@@ -13,7 +14,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <FruitList />
+      <Form />
     </div>
   );
 }
