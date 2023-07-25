09aecff0ff26d2e35bbe98af37480c98e2e6db06
Mo <folarinfadeyi@gmail.com>
2023-07-25 14:44:51 -0700

    Minimal touches

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Column from './Column'
 
 function Table() {
   return (
     <Table>
         <tr>
-            
+            <Column />
         </tr>
     </Table>
   )
