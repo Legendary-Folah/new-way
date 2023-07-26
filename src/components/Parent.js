4637947f8667d84ea819c9f273d6a6cecaf1ce5c
Mo <folarinfadeyi@gmail.com>
2023-07-25 20:02:54 -0700

    okaaay

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,9 +2,13 @@ import React from 'react'
 import Child from './Child';
 
 function Parent() {
+
+    
+
   return (
     <div>
         This is a Parent Component
+        <h2>Parent Count is {}</h2>
         <Child />
     </div>
   )
